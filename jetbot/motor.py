164b71aa51edd5470eadbbedb54a59053ad09f00
Aayush Naik <aayushnaik17@gmail.com>
2019-12-24 15:58:13 -0800

    Improve motor documentation

--- jetbot/motor.py
+++ jetbot/motor.py
@@ -3,6 +3,14 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT
 
 
 class Motor(object):
+    """Used to update speed of the Jetbot motors.
+    
+    Args:
+        driver: An `Adafruit_MotorHAT` instance used to control the motor.
+        channel: Motor channel. Left is channel 1 and right is channel 2.
+        alpha: Motor configuration parameter.
+        beta: Motor configuration parameter.
+    """
     def __init__(self, driver, channel, alpha=1.0, beta=0.0, *args, **kwargs):
         self._driver = driver
         self._motor = self._driver.getMotor(channel)
