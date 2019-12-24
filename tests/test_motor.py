e50184a848aa3c10b787718a1aa85a562d153515
Aayush Naik <aayushnaik17@gmail.com>
2019-12-24 15:51:08 -0800

    Make motor traitlets-free

--- tests/test_motor.py
+++ tests/test_motor.py
@@ -1,16 +1,29 @@
 from jetbot import Motor
+import random
 
 
 class TestMotor:
-    def test_left_forward(self):
-        """Test left motor's forward motion."""
+    def test_motion(self):
+        """Test left/right motor's motion."""
         try:
             from Adafruit_MotorHAT import Adafruit_MotorHAT
             channel = 1
             driver = Adafruit_MotorHAT(i2c=1)
             motor = Motor(driver, channel=channel, alpha=1.0)
-            value = -0.5  # value in [-1, 1]
-            motor.update_value(value)
+            # value in [-1, 1]
+            for value in ((2 * random.random() - 1) for _ in range(5)):
+                motor.update_value(value)
         except Exception as e:
-            print(e)
+            print(f'Test failed:\n {e}')
+    
+    def test_release(self):
+        """Test motor's release function."""
+        try:
+            from Adafruit_MotorHAT import Adafruit_MotorHAT
+            channel = 1
+            driver = Adafruit_MotorHAT(i2c=1)
+            motor = Motor(driver, channel=channel, alpha=1.0)
+            motor._release()
+        except Exception as e:
+            print(f'Test failed:\n {e}')
             
