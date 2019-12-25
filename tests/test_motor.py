029913a64b9708bb216904e3d7bb445d0232f9d6
Aayush Naik <aayushnaik17@gmail.com>
2019-12-24 16:30:03 -0800

    Fix typo

--- tests/test_motor.py
+++ tests/test_motor.py
@@ -7,7 +7,7 @@ class TestMotor:
         """Test left/right motor's motion."""
         from Adafruit_MotorHAT import Adafruit_MotorHAT
         channel = 1
-        driver = Adafruit_MotorHAT(i2c=1)
+        driver = Adafruit_MotorHAT(i2c_bus=1)
         motor = Motor(driver, channel=channel, alpha=1.0)
         # value in [-1, 1]
         for value in ((2 * random() - 1) for _ in range(5)):
@@ -17,7 +17,7 @@ class TestMotor:
         """Test motor's release function."""
         from Adafruit_MotorHAT import Adafruit_MotorHAT
         channel = 1
-        driver = Adafruit_MotorHAT(i2c=1)
+        driver = Adafruit_MotorHAT(i2c_bus=1)
         motor = Motor(driver, channel=channel, alpha=1.0)
         motor._release()
             
